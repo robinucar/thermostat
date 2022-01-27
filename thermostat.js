ba6aabb5c493bcbda735f37f52837db00dc427e2
robin ucar <mehmet.ucar@imediapro.co.uk>
2022-01-27 21:37:18 +0000

    increase temperature

--- thermostat.js
+++ thermostat.js
@@ -1,12 +1,19 @@
 class Thermostat {
-    constructor() {
-      this.temperature = 20;
-    }
-    getTemperature() {
-        return this.temperature;
-      }
+  constructor() {
+    this.temperature = 20;
+  }
+  getTemperature() {
+    return this.temperature;
+  }
+
+  up() {
+    this.temperature++
+  }
 }
 
+module.exports = Thermostat;
 
-
-  module.exports = Thermostat;
+const t = new Thermostat()
+console.log(t)
+t.up()
+console.log(t)
--- thermostat.test.js
+++ thermostat.test.js
@@ -1,9 +1,16 @@
-const Thermostat = require('./thermostat')
-describe('Thermostat', () => {
-  
-    // The Thermostat starts with an initial temperature of 20 degrees
-    it('has a default temperature of 20', () => {
-      const thermostat = new Thermostat();
-      expect(thermostat.getTemperature()).toEqual(20);
-    });
-})
+const Thermostat = require("./thermostat");
+describe("Thermostat", () => {
+  // The Thermostat starts with an initial temperature of 20 degrees
+  it("has a default temperature of 20", () => {
+    const thermostat = new Thermostat();
+    expect(thermostat.getTemperature()).toEqual(20);
+  });
+
+  // You can increase the temperature with an up method
+  it('increases temperature by 1', () => {
+    const thermostat = new Thermostat();
+    // increase temprature
+    thermostat.up()
+    expect(thermostat.getTemperature()).toEqual(21)
+  })
+});
